172cd1b4a83ed21e929c0870f7328011a73dd97b
Raycode-03 <akereleolasun5@gmail.com>
2026-03-26 15:18:06 +0100

    first commit hotelXpace

--- frontend/app/loading.tsx
+++ frontend/app/loading.tsx
@@ -1,9 +1,9 @@
 "use client";
-import React, { useEffect, useState } from "react";
-import Image from "next/image";
-
+import React, { useEffect, useState, useRef } from "react";
+import gsap from "gsap/all";
 function Loading() {
   const [progress, setProgress] = useState(0);
+  const [showXpace, setShowXpace] = useState(false);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -15,21 +15,55 @@ function Loading() {
     return () => clearInterval(interval);
   }, []);
 
-  return (
-    <main className="fixed inset-0 z-50 bg-black flex items-center justify-center">
-      <div className="relative w-60 h-60 flex items-center justify-center">
-        {/* Glow */}
-        <div className="absolute inset-0 blur-2xl opacity-30">
-          <div className="w-full h-full bg-gradient-to-br from-blue-500 to-blue-800 rounded-full animate-pulse" />
-        </div>
+  const step1 = () => {
+    gsap.fromTo(
+      ".hotel", 
+      {opacity:0},
+      {
+      duration: 0.8,
+      scale: 1.2,
+      opacity: 1,
+       x: -20, 
+      ease: "power1.inOut",
+       onComplete: () => {
+        
+        gsap.set(".xpace", { visibility: "visible" });
+        gsap.fromTo(
+      ".xpace",
+      { opacity: 0, x: 30 },
+      { opacity: 1, x: 0, duration: 0.5, ease: "power1.inOut" }
+    );
+    },
+  }
+)
+  };
 
-        <Image
+  useEffect(() => {
+    step1();
+  }, []);
+  return (
+    <main className="fixed inset-0 z-50 bg-black flex items-center justify-center overflow-hidden">
+      <div className="relative flex items-center justify-center">
+        {/* Glow */}
+        {/* <div className="absolute inset-0 blur-2xl opacity-30">
+          <div className="w-full h-full bg-gradient-to-br from-blue-500 to-blue-800 rounded-full animate-pulse" />
+        </div> */}
+         {/* <Image
           src="/logos/hotelxpace.svg"
           alt="Loading"
           fill
           unoptimized
-          className="object-contain invert animate-float"
-        />
+          className="object-contain animate-float"
+        />  */}
+
+        <h1 className="text-gold text-5xl md:text-7xl lg:text-9xl flex items-center">
+          <span className="hotel">Hotel</span>
+          <span
+            className="xpace opacity-0 invisible"
+          >
+            Xpace
+          </span>
+        </h1>
       </div>
     </main>
   );
